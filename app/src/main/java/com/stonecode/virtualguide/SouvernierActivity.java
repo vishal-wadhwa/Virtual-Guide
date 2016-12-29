a7a040ad8b9872d077249e1c7a8fd6f4ff634cbd
Dhruv Rathi <dhruvrathi15@gmail.com>
2016-12-29 15:19:07 +0530

    Ticket Activity

--- app/src/main/java/com/stonecode/virtualguide/SouvernierActivity.java
+++ app/src/main/java/com/stonecode/virtualguide/SouvernierActivity.java
@@ -1,16 +1,17 @@
 package com.stonecode.virtualguide;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class SouvernierActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Intent i = new Intent(MainActivity.this,TicketActivity.class);
-        startActivity(i);
+        setContentView(R.layout.activity_souvernier);
+
+
+
+
     }
 }
